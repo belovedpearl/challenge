5021521199a868e8f7766f9d45dbdebf884cec1e
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-11-06 03:56:32 +0000

    My first React task

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SideBar(){
+    return (
+        <div> 
+            <a href='#'>My Photos</a> 
+            <a href='#'>My Illustrations</a> 
+            <a href='#'>My Paintings</a> 
+            <img/> 
+        </div>
+            )
+}
+export default SideBar
+    
