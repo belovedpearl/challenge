dace44c7b839a4ff61524ea066b8b86a59511ec7
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-11-08 14:48:26 +0000

    Edited task2

--- my-app/src/components/NavBarSimple.js
+++ my-app/src/components/NavBarSimple.js
@@ -5,34 +5,31 @@ class NavBarSimple extends React.Component{
    constructor(props) {
     super(props);
     this.state = {
-        name : 'Hello, guest!',
+        message : 'Hello, guest!',
         buttonText : 'Log in',
     }
    }
-   handleClick() {
-    this.setState((prevState, prevProps) => {
-      console.log("Previous State:", prevState);
-      console.log("Previous Props:", prevProps);
-      return {
-        name:
-          prevState.name === "Hello, guest!" ? "Welcome back user!" : "Hello, guest!",
-          buttonText: prevState.buttonText === "Log in" ? "Log out" : "Log in",
-      };
-    });
-  }
 
-    render(){
-        return (
-            <div className={css.NavBar}> 
-                <h1>My Gallery</h1>
-                <div>
-                    <span>{this.state.name} </span>
-                    <button onClick= { () => this.handleClick() }>
-                        {this.state.buttonText}
-                    </button>
-                </div>
-            </div>
-                )}
+   handleClick = () => {
+    this.setState((prevState) => ({
+        message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+        buttonText: prevState.buttonText === "Log out" ? "Log in" : "Log out",
+    }), ()=> console.log(this.state.message))
+    
+}
+ 
+  render(){
+      return (
+          <div className={css.NavBar}> 
+              <h1>My Gallery</h1>
+              <div>
+                  <span>{this.state.message} </span>
+                  <button onClick= { () => this.handleClick() }>
+                      {this.state.buttonText}
+                  </button>
+              </div>
+          </div>
+              )}
 }
 
 export default NavBarSimple
