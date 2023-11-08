bd42d7c9fa86e737832c3cd6c8062e439c81cebf
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-11-08 14:36:02 +0000

    Add navbar with state challenge

---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import css from "./css/NavBarSimple.module.css";
+
+class NavBarSimple extends React.Component{
+   constructor(props) {
+    super(props);
+    this.state = {
+        name : 'Hello, guest!',
+        buttonText : 'Log in',
+    }
+   }
+   handleClick() {
+    this.setState((prevState, prevProps) => {
+      console.log("Previous State:", prevState);
+      console.log("Previous Props:", prevProps);
+      return {
+        name:
+          prevState.name === "Hello, guest!" ? "Welcome back user!" : "Hello, guest!",
+          buttonText: prevState.buttonText === "Log in" ? "Log out" : "Log in",
+      };
+    });
+  }
+
+    render(){
+        return (
+            <div className={css.NavBar}> 
+                <h1>My Gallery</h1>
+                <div>
+                    <span>{this.state.name} </span>
+                    <button onClick= { () => this.handleClick() }>
+                        {this.state.buttonText}
+                    </button>
+                </div>
+            </div>
+                )}
+}
+
+export default NavBarSimple
